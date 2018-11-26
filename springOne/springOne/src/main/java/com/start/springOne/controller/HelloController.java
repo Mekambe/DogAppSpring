466639ffd20647df66db8c5afbcc165d75d56a91
JMisiorny <mekambe2@gmail.com>
2018-11-26 21:00:21 +0100

    findh2

--- springOne/springOne/src/main/java/com/start/springOne/controller/HelloController.java
+++ springOne/springOne/src/main/java/com/start/springOne/controller/HelloController.java
@@ -1,12 +1,24 @@
 package com.start.springOne.controller;
 
 import com.start.springOne.dto.PersonDto;
+import com.start.springOne.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController // spring ma odczytac to tak jakbym hostowal
 public class HelloController {
+
+    private PersonRepository personRepository;
+
+    @Autowired
+    public HelloController(PersonRepository personRepository) {
+        this.personRepository = personRepository;
+    }
 
     @RequestMapping ("/hello")// pod jakim adresem ma byc dostepna usluga, endpointer
     public String hello (){
@@ -15,10 +27,27 @@ public class HelloController {
     }
 
     @RequestMapping("/person")
-    public PersonDto personDto
-            (@RequestParam(value="name") String name){
+    public Optional<PersonDto> findById
+            (@RequestParam(value="id") Long id){
 
-        return new PersonDto(10,name);
+        return personRepository.findById(id);
     }
+
+    @RequestMapping("/save")
+    public @ResponseBody String saveUser
+            (@RequestParam(value = "firstName") String firstName,@RequestParam("age") int age, @RequestParam(value = "address")
+             String address, @RequestParam(value = "sex") String sex) {
+
+        PersonDto n = new PersonDto();
+        n.setAddress(address);
+        n.setAge(age);
+        n.setFirstName(firstName);
+        n.setSex(sex);
+
+        personRepository.save(n);
+        return "Save";
+
+    }
+
 
 }
--- springOne/springOne/src/main/java/com/start/springOne/dto/PersonDto.java
+++ springOne/springOne/src/main/java/com/start/springOne/dto/PersonDto.java
@@ -1,27 +1,73 @@
 package com.start.springOne.dto;
 
-public class PersonDto {
-    private int id;
-    private String name;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
-    public int getId() {
+@Entity // znaczy to ze jedna klase ma zamapowac na abze danych
+public class PersonDto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO) // spring sam sobie dobierze autoicrement / generated value znaczy ze bedzie unikatowe id tworzyl
+    private Long id;
+    private String firstName;
+    private int age;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public Long getId() {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
-    public String getName() {
-        return name;
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    @Override
+    public String toString() {
+        return "PersonDto{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", age=" + age +
+                ", address='" + address + '\'' +
+                ", sex='" + sex + '\'' +
+                '}';
     }
 
-    public PersonDto(int id, String name) {
-        this.id = id;
-        this.name = name;
+    public int getAge() {
+        return age;
     }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    private String address;
+    private String sex;
+
+
+
 }
